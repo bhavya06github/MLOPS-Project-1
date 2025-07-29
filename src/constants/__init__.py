9a843338adcf0e50e7c890a2f27d5c7ef9cb7a35
Bhavya <bhavyaantil06@gmail.com>
2025-07-29 15:13:06 +0530

    Model Trainer added (a few extra files for next step added)

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -24,7 +24,10 @@ TEST_FILE_NAME:str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join('config','schema.yaml')
 
 
-# AWS PENDING
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
 
 '''
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -48,29 +51,29 @@ DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
-# """
-# Model Trainer related constant start with MODEL_TRAINER VAR NAME
-# """
-# MODEL_TRAINER_DIR_NAME: str = "model_trainer"
-# MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
-# MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
-# MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-# MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
-# MODEL_TRAINER_N_ESTIMATORS = 200
-# MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 7
-# MODEL_TRAINER_MIN_SAMPLES_LEAF: int = 6 
-# MIN_SAMPLES_SPLIT_MAX_DEPTH: int = 10 
-# MIN_SAMPLES_SPLIT_CRITERION: str = "entropy"
-# MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
+"""
+Model Trainer related constant start with MODEL_TRAINER VAR NAME
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+MODEL_TRAINER_N_ESTIMATORS = 200
+MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 7
+MODEL_TRAINER_MIN_SAMPLES_LEAF: int = 6 
+MIN_SAMPLES_SPLIT_MAX_DEPTH: int = 10 
+MIN_SAMPLES_SPLIT_CRITERION: str = "entropy"
+MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 
 
-# """
-# MODEL Evaluation related constants
-# """
-# MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-# MODEL_BUCKET_NAME = "my-model-mlopsproj"
-# MODEL_PUSHER_S3_KEY = "model-registry"
+"""
+MODEL Evaluation related constants
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "my-model-mlopsproj"
+MODEL_PUSHER_S3_KEY = "model-registry"
 
 
-# APP_HOST = "0.0.0.0"
-# APP_PORT = 5000
+APP_HOST = "0.0.0.0"
+APP_PORT = 5000
